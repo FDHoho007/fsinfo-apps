c5a6db49e95ed2d6324219d1f9388ebb98da7b90
Alexander Degenhart <degenhart@fim.uni-passau.de>
2020-05-30 08:45:52 +0000

    Fix curl not working, by removing the curl calls
    file_get_contents is fast enough for our usage.
    
    Lesson learned:
    When testing always test all code paths ;)

--- bbb-status.php
+++ bbb-status.php
@@ -5,15 +5,8 @@ $bbbroom = $_GET['room'];
 $url = "https://fsinfo.uni-passau.de/" . $bbbroom;
 
 try {
-    if (in_array ('curl', get_loaded_extensions())) {
-        $c = curl_init($url);
-        $h[] = "Accept-Language: en,en-US";
-        curl_setopt($c, CURLOPT_HTTPHEADER, $h);
-        $content = curl_exec($c);
-    } else {
-        $context = stream_context_create(array('http' => array('header'=>'Connection: close\r\nAccept-language: en,en-US\r\n')));
-        $content = file_get_contents($url,false,$context);
-    }
+    $context = stream_context_create(array('http' => array('header'=>'Connection: close\r\nAccept-language: en,en-US\r\n')));
+    $content = file_get_contents($url,false,$context);
     if ($content === false) {
         // Handle the error
         echo "Error";
